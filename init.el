87fc80f49c5cad031855f182a25155fd6f382600
Lucio Franco <luciofranco14@gmail.com>
2018-10-01 01:07:36 -0400

    Update more use-package configs

--- init.el
+++ init.el
@@ -1,5 +1,10 @@
+(setq debug-on-error t)
+
 (require 'package)
 (require 'use-package)
+
+;; Skip the default splash screen.
+(setq inhibit-startup-message t)
 
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
@@ -8,7 +13,6 @@
 (setq rust-format-on-save t)
 
 (projectile-mode +1)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 (load-theme 'monokai t)
@@ -19,7 +23,14 @@
 (global-display-line-numbers-mode)
 
 (use-package magit
-  :bind ("C-c g" magit-status))
+  :bind (("C-c g" . magit-status)))
 
 (use-package ace-window
-  :bind ("C-." . ace-window))
+  :bind (("C-." . ace-window)))
+
+(use-package dimmer)
+
+(use-package rust)
+
+(use-package helm
+  :bind ("M-x" . helm-M-x))
