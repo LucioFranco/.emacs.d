b1aa84935bcf4d20add799075509fa28e847ad7e
Lucio Franco <luciofranco14@gmail.com>
2018-11-20 11:48:53 -0500

    Add solarized and cleaned up shackle config

--- init.el
+++ init.el
@@ -101,6 +101,9 @@
 (use-package zenburn-theme
   :defer t)
 
+(use-package solarized-theme
+  :defer t)
+
 (load-theme 'zenburn t)
 
 
@@ -141,22 +144,9 @@
 
     (setq shackle-rules
           ;; CONDITION(:regexp)            :select     :inhibit-window-quit   :size+:align|:other     :same|:popup
-          '((compilation-mode              :select nil                        :same t                       )
-            ("*undo-tree*"                                                    :size 0.25 :align right)
-            ("*eshell*"                    :select t                          :other t               )
-            ("*Shell Command Output*"      :select nil                                               )
-            ("\\*Async Shell.*\\*" :regexp t :ignore t                                                 )
-            (occur-mode                    :select nil                                   :align t    )
-            ("*Help*"                      :select t   :inhibit-window-quit t :other t               )
-            ("*Completions*"                                                  :size 0.3  :align t    )
-            ("*Messages*"                  :select nil :inhibit-window-quit t :other t               )
-            ("\\*[Wo]*Man.*\\*"    :regexp t :select t   :inhibit-window-quit t :other t               )
-            ("\\*poporg.*\\*"      :regexp t :select t                          :other t               )
-            ("\\`\\*helm.*?\\*\\'"   :regexp t                                    :size 0.3  :align t    )
-            ("*Calendar*"                  :select t                          :size 0.3  :align below)
-            ("*info*"                      :select t   :inhibit-window-quit t                         :same t)
-            (magit-status-mode             :select t   :inhibit-window-quit f  :same f)
-            (magit-log-mode                :select t   :inhibit-window-quit f  :same f)
+          '((compilation-mode :select nil :same t)
+            (magit-status-mode :select nil :same t)
+            (magit-log-mode :select nil :same t)
             ))
 
     (shackle-mode 1)))
@@ -595,4 +585,4 @@
 (message "Done loading configuration!")
 
 (provide 'init)
-;p;; init.el ends here
+;;; init.el ends here
