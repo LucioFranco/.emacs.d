732c91f8985249b74305f99315a7ac024f29c7ee
Lucio Franco <luciofranco14@gmail.com>
2018-12-10 19:57:52 -0500

    Update emacs.d

--- init.el
+++ init.el
@@ -104,7 +104,13 @@
 (use-package solarized-theme
   :defer t)
 
-(load-theme 'zenburn t)
+(use-package organic-green-theme
+  :defer t)
+
+;; (use-package alect-light
+;;   :defer t)
+
+(load-theme 'solarized-dark t)
 
 
 (global-hl-line-mode +1)
@@ -586,3 +592,17 @@
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("45b84ddcc65bdf01d9cc76061a9473e3291d82c9209eac0694fbbb81c57b92fd" "63e6337545edc4f404ea04a756d369e36e9974b5e7b85cce7cbc246552647516" "2925ed246fb757da0e8784ecf03b9523bccd8b7996464e587b081037e0e98001" "c1c0899fe31d75e82286d64c14d3a858636fc759cd3c78139c3038f7330041db" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
